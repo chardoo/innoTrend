bb06bdbf8e415d3448fdbc0e33c5c78cc4d15590
chardoo <40030004+chardoo@users.noreply.github.com>
2025-11-27 23:29:09 +0100

    Update CORS middleware to allow all origins

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ middleware = [
 app = FastAPI(title=settings.APP_NAME,middleware=middleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOW_ORIGINS,
+    allow_origins=['*'],
     allow_methods=["*"],
     allow_headers=["*"],
 )
